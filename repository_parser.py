c7e0d8fcc0712c4b95cebbe72b0acfa85149e1ab
zhouzhenliang <zhouzhenliangpp@163.com>
2023-12-11 21:19:39 +0800

    下载matadata.xml

--- repository_parser.py
+++ repository_parser.py
@@ -1,6 +1,22 @@
+import os
+
+import requests
+
 if __name__ == '__main__':
-    maven = 'https://dl.google.com/dl/android/maven2/'
+    maven_url = 'https://dl.google.com/dl/android/maven2/'
+    maven_path = '.m'
+    metadata = 'maven-metadata.xml'
+
     path = 'androidx.core:core-ktx:1.12.0'
     group, artifact, version = path.split(":")
-    metadata = '/'.join(group.split('.')) + f'/{artifact}/maven-metadata.xml'
-    print(f'{maven}/{metadata}')
+    maven_dir = '/'.join(group.split('.')) + f'/{artifact}'
+
+    metadata_url = f'{maven_url}/{maven_dir}/{metadata}'
+    metadata_path = f'{maven_path}/{maven_dir}/{metadata}'
+    print(metadata_url)
+    print(metadata_path)
+
+    metadata_file = requests.get(metadata_url)
+    os.makedirs(os.path.dirname(metadata_path), exist_ok=True)
+    with open(metadata_path, 'wb') as file:
+        file.write(metadata_file.content)

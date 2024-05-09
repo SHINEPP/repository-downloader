9afddbb2bc8dc1d80f25b532963a1c8dbb0cbe94
zhouzhenliang <zhouzhenliangpp@163.com>
2024-05-09 12:02:58 +0800

    下载三方保活库

--- maven/maven_implementation.py
+++ maven/maven_implementation.py
@@ -9,11 +9,32 @@ class MavenImplementation:
     def _parser(self):
         self.group_id, self.artifact_id, self.version = self.path.split(":")
 
-    def maven_metadata_path(self):
-        return '/'.join(self.group_id.split('.')) + f'/{self.artifact_id}' + '/maven-metadata.xml'
+    def group_path(self):
+        return '/'.join(self.group_id.split('.'))
+
+    def artifact_path(self):
+        return f'{self.group_path()}/{self.artifact_id}'
+
+    def version_path(self):
+        return f'{self.artifact_path()}/{self.version}'
+
+    def metadata_path(self):
+        return f'{self.artifact_path()}/maven-metadata.xml'
+
+    def pom_path(self):
+        return f'{self.version_path()}/{self.artifact_id}-{self.version}.pom'
+
+    def pom_sha1_path(self):
+        return f'{self.pom_path()}.sha1'
+
+    def aar_path(self):
+        return f'{self.version_path()}/{self.artifact_id}-{self.version}.aar'
+
+    def aar_sha1_path(self):
+        return f'{self.aar_path()}.sha1'
 
 
 if __name__ == '__main__':
-    impl = MavenImplementation('com.google.ads.mediation:mintegral:16.5.21.0')
+    impl = MavenImplementation('com.clover.hdwallpapers.album:vyajpe:v30-jiagu')
     print(f'path = {impl.group_id}:{impl.artifact_id}:{impl.version}')
-    print(f'maven_metadata_path = {impl.maven_metadata_path()}')
+    print(f'maven_metadata_path = {impl.pom_path()}')
--- maven/maven_repository.py
+++ maven/maven_repository.py
@@ -2,8 +2,8 @@ from requests.auth import HTTPBasicAuth
 
 
 class MavenRepository:
-    def __init__(self, host, user_name, password):
-        self.url = host
+    def __init__(self, url, user_name, password):
+        self.url = url
         self.user_name = user_name
         self.password = password
 
@@ -14,10 +14,43 @@ class MavenRepository:
 
 
 if __name__ == '__main__':
+    import os
     import requests
+    import hashlib
+    from maven_implementation import MavenImplementation
 
-    url = 'https://maven.cherrysoft.cn/repository/maven-releases/' + 'com/oh/bb/mmkv/develop-gp/9.5.1/develop-gp-9.5.1.module'
-    repository = MavenRepository(url, 'develop', 'qwert12345')
-    response = requests.get(repository.url, auth=repository.http_basic_auth())
+    resp_url = 'https://packages.aliyun.com/maven/repository/2279663-release-AiyNZM'
+    repository = MavenRepository(resp_url, '621344be8af5d39eb3f17f3e', '(ijI1DwR7[wG')
+    impl = MavenImplementation('com.clover.hdwallpapers.album:vyajpe:v30-jiagu')
+
+    pom_url = f'{resp_url}/{impl.pom_path()}'
+    print(f'pom_url = {pom_url}')
+    response = requests.get(pom_url, auth=repository.http_basic_auth())
+    file_path = f'../.m/{impl.pom_path()}'
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+    pom_sha1 = hashlib.new("sha1", response.content).hexdigest()
+    print(f'pom_sha1 = {pom_sha1}')
+
+    pom_sha1_url = f'{resp_url}/{impl.pom_sha1_path()}'
+    print(f'pom_sha1_url = {pom_sha1_url}')
+    response = requests.get(pom_sha1_url, auth=repository.http_basic_auth())
+    text = response.text
+    print(text)
+
+    aar_url = f'{resp_url}/{impl.aar_path()}'
+    print(f'aar_url = {aar_url}')
+    response = requests.get(aar_url, auth=repository.http_basic_auth())
+    file_path = f'../.m/{impl.aar_path()}'
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+    aar_sha1 = hashlib.new("sha1", response.content).hexdigest()
+    print(f'aar_sha1 = {aar_sha1}')
+
+    aar_sha1_url = f'{resp_url}/{impl.aar_sha1_path()}'
+    print(f'aar_sha1_url = {aar_sha1_url}')
+    response = requests.get(aar_sha1_url, auth=repository.http_basic_auth())
     text = response.text
     print(text)

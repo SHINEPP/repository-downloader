0f35f34f8b421792129f4797b8b88552777514a7
zhouzhenliang <zhouzhenliangpp@163.com>
2024-10-16 12:10:04 +0800

    优化sync maven代码，可以选择不sync依赖库 & 支持需要登录的maven

--- repository_sync.py
+++ repository_sync.py
@@ -4,6 +4,31 @@ import time
 import xml.etree.ElementTree as etree
 
 import requests
+from requests.auth import HTTPBasicAuth
+
+fingerprint = ['md5', 'sha1', 'sha256', 'sha512']
+
+
+def maven_download_file(host: dict, local, path):
+    print(f'download: {host["uri"]}{path}', end='')
+    auth = None
+    if 'credentials' in host.keys():
+        credentials = host['credentials']
+        if 'username' in credentials.keys() and 'password' in credentials.keys():
+            username = credentials['username']
+            password = credentials['password']
+            if username and password:
+                auth = HTTPBasicAuth(username, password)
+    response = requests.get(os.path.join(host['uri'], path), auth=auth)
+    if response.status_code == 200:
+        local_path = local + path
+        os.makedirs(os.path.dirname(local_path), exist_ok=True)
+        with open(local_path, 'wb') as file:
+            file.write(response.content)
+            print(' -> success')
+        return response
+    print(' -> fail')
+    return None
 
 
 class MavenMetadata:
@@ -60,7 +85,7 @@ class MavenPom:
     def _parser(self, content: str):
         root = etree.fromstring(content)
         ns = ''
-        result = re.match(r'(\{.+\}).+', root.tag)
+        result = re.match(r'(\{.+}).+', root.tag)
         if result:
             ns = result.group(1)
         if root.tag != ns + 'project':
@@ -165,21 +190,7 @@ class Implementation:
         self.root_dir = '/'.join(self.group_id.split('.')) + f'/{self.artifact_id}'
 
 
-def download_file(host, local, path):
-    print(f'download: {host}{path}', end='')
-    response = requests.get(host + path)
-    if response.status_code == 200:
-        local_path = local + path
-        os.makedirs(os.path.dirname(local_path), exist_ok=True)
-        with open(local_path, 'wb') as file:
-            file.write(response.content)
-            print(' -> success')
-        return response
-    print(' -> fail')
-    return None
-
-
-class SyncImplementation:
+class ImplementationSyncer:
 
     def __init__(self, path):
         self.implementation = Implementation(path)
@@ -197,9 +208,11 @@ class SyncImplementation:
                 with open(local_metadata, 'r') as file:
                     metadata_text = file.read()
         if len(metadata_text) == 0:
-            metadata_resp = download_file(host, local, metadata_path)
+            metadata_resp = maven_download_file(host, local, metadata_path)
             if metadata_resp:
                 metadata_text = metadata_resp.text
+                for name in fingerprint:
+                    maven_download_file(host, local, metadata_path + '.' + name)
         if len(metadata_text) > 0:
             self.metadata = MavenMetadata(metadata_text)
 
@@ -214,11 +227,11 @@ class SyncImplementation:
             with open(local_pom, 'r') as file:
                 pom_text = file.read()
         if len(pom_text) == 0:
-            pom_resp = download_file(host, local, pom_path)
+            pom_resp = maven_download_file(host, local, pom_path)
             if pom_resp:
                 pom_text = pom_resp.text
                 for name in fingerprint:
-                    download_file(host, local, pom_path + '.' + name)
+                    maven_download_file(host, local, pom_path + '.' + name)
         if len(pom_text) > 0:
             self.pom = MavenPom(self.implementation, pom_text)
 
@@ -230,39 +243,63 @@ class SyncImplementation:
         local_artifact = local + artifact_path
         if os.path.exists(local_artifact):
             return True
-        artifact_resp = download_file(host, local, artifact_path)
+        artifact_resp = maven_download_file(host, local, artifact_path)
         if artifact_resp:
             for name in fingerprint:
-                download_file(host, local, artifact_path + '.' + name)
+                maven_download_file(host, local, artifact_path + '.' + name)
             source_jar_url = self.pom.maven_source_jar_path()
-            source_jar_resp = download_file(host, local, source_jar_url)
+            source_jar_resp = maven_download_file(host, local, source_jar_url)
             if source_jar_resp:
                 for name in fingerprint:
-                    download_file(host, local, source_jar_url + '.' + name)
+                    maven_download_file(host, local, source_jar_url + '.' + name)
             return True
         return False
 
 
-def start_sync(path):
-    if path in paths:
-        return
-    print(f'sync: {path}')
-    paths.append(path)
-    sync = SyncImplementation(path)
-    for host in maven_hosts:
-        if sync.sync_artifact(host, maven_local_dir):
-            for depe in sync.pom.dependencies:
-                depe_path = depe.group_id + ':' + depe.artifact_id + ':' + depe.version
-                start_sync(depe_path)
-            break
+class Syncer:
+    def __init__(self, hosts: list, local_dir: str, sync_den: bool = True):
+        self.paths = []
+        self.hosts = hosts
+        self.local_dir = local_dir
+        self.sync_den = sync_den
+
+    def start_sync(self, path):
+        if path in self.paths:
+            return
+        print(f'sync: {path}')
+        self.paths.append(path)
+        sync = ImplementationSyncer(path)
+        for host in self.hosts:
+            if sync.sync_artifact(host, self.local_dir):
+                if not self.sync_den:
+                    continue
+                for depe in sync.pom.dependencies:
+                    depe_path = depe.group_id + ':' + depe.artifact_id + ':' + depe.version
+                    self.start_sync(depe_path)
+                break
 
 
 if __name__ == '__main__':
-    fingerprint = ['md5', 'sha1', 'sha256', 'sha512']
-    maven_hosts = ['https://dl.google.com/dl/android/maven2/',
-                   'https://repo1.maven.org/maven2/',
-                   'https://jcenter.bintray.com/']
-    maven_local_dir = '.m/'
+    maven_hosts = [
+        {'uri': 'https://dl.google.com/dl/android/maven2/'},
+        {'uri': 'https://repo1.maven.org/maven2/'},
+        {'uri': 'https://jcenter.bintray.com/'},
+        {'uri': 'https://jitpack.io/'},
+        {'uri': 'https://maven.aliyun.com/repository/google/'},
+        {'uri': 'https://maven.aliyun.com/repository/public/'},
+        {
+            'uri': 'https://maven.cherrysoft.cn/repository/maven-releases/',
+            'credentials': {
+                'username': 'develop',
+                'password': 'qwert12345'
+            }
+        }
+    ]
 
-    paths = []
-    start_sync('com.google.ads.mediation:mintegral:16.5.21.0')
+    syncer = Syncer(hosts=maven_hosts, local_dir='./.m/', sync_den=False)
+    syncer.start_sync('com.oh.tinker.tinker-android:tinker-android-lib:1.0.1')
+
+    # start_sync('eu.davidea:flexible-adapter:5.1.0', sync_den=False)
+    # start_sync('eu.davidea:flexible-adapter-ui:1.0.0')
+    # start_sync('eu.davidea:flexible-adapter-livedata:1.0.0-b3')
+    # start_sync('eu.davidea:flexible-adapter-databinding:1.0.0')

e22c21ce70716acb9d3840448905b16f9593f975
zhouzhenliang <zhouzhenliangpp@163.com>
2024-11-18 11:02:42 +0800

    checksum

--- repository_checksum.py
+++ repository_checksum.py
@@ -2,36 +2,24 @@ import hashlib
 import os.path
 
 
-def gen_check_sum(path: str):
-    path1, path2 = os.path.splitext(path)
-
-    # aar
+def gen_check_files(path: str):
     with open(path, 'rb') as file:
         buffer = file.read()
-    md5 = hashlib.md5(buffer).hexdigest()
-    sha1 = hashlib.sha1(buffer).hexdigest()
-    sha256 = hashlib.sha256(buffer).hexdigest()
     with open(path + '.md5', 'w') as file:
-        file.write(md5)
+        file.write(hashlib.md5(buffer).hexdigest())
     with open(path + '.sha1', 'w') as file:
-        file.write(sha1)
+        file.write(hashlib.sha1(buffer).hexdigest())
     with open(path + '.sha256', 'w') as file:
-        file.write(sha256)
+        file.write(hashlib.sha256(buffer).hexdigest())
 
-    # pom
-    with open(path1 + '.pom', 'rb') as file:
-        buffer = file.read()
-    md5 = hashlib.md5(buffer).hexdigest()
-    sha1 = hashlib.sha1(buffer).hexdigest()
-    sha256 = hashlib.sha256(buffer).hexdigest()
-    with open(path1 + '.pom.md5', 'w') as file:
-        file.write(md5)
-    with open(path1 + '.pom.sha1', 'w') as file:
-        file.write(sha1)
-    with open(path1 + '.pom.sha256', 'w') as file:
-        file.write(sha256)
+
+def gen_check_sum(path: str):
+    path1, path2 = os.path.splitext(path)
+    gen_check_files(path)
+    gen_check_files(path1 + '.pom')
+    gen_check_files(os.path.join(os.path.dirname(os.path.dirname(path)), 'maven-metadata.xml'))
 
 
 if __name__ == '__main__':
     gen_check_sum(
-        '/Users/zhouzhenliang/Desktop/temp/topon_ad_gen/com/anythink/sdk/interstitial-tpn/6.4.18/interstitial-tpn-6.4.18.aar')
+        '/Users/zhouzhenliang/Desktop/temp/topon_log/com/anythink/sdk/interstitial-tpn-alpha/6.4.17/interstitial-tpn-alpha-6.4.17.aar')
